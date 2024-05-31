d2dc05ce67b519465b1c270ae30bc0665db1353c
Ekaterina Voitsekhovich <kate06042003@gmail.com>
2024-05-31 17:36:08 +0300

    31.05.2024 commit

--- modules/pages_handlers/admins_handlers/change_reg_status_handler.php
+++ modules/pages_handlers/admins_handlers/change_reg_status_handler.php
@@ -2,6 +2,10 @@
 if(session_status()!=PHP_SESSION_ACTIVE) session_start();
 include '..\..\..\connect\connect_database.php';
 
+require_once __DIR__ .'../../../../mail/vendor/autoload.php';
+$settings = require_once __DIR__ .'../../../../mail/settings.php';
+require_once __DIR__ .'../../../../mail/functions.php';
+
 $reg_status_id=$_POST['reg_status'];
 $id_status_select=$_POST['id_status_select'];
 
@@ -15,6 +19,54 @@ if ($result) {
 
     $row = mysqli_fetch_row($result2); 
     $status=$row[1];
+
+    $findQuery = "SELECT Organized_course.ID as id_orgCourse, Organized_course.startDate, User.name, Course.title, Course.price, Group_type.groupType, Group_type.priceCoefficient, Course_registration.ID_user as user_id FROM Course_registration JOIN Organized_course ON Course_registration.ID_organizedCourse=Organized_course.ID JOIN Course ON Organized_course.ID_course=Course.ID JOIN Master ON Organized_course.ID_master=Master.ID JOIN User ON User.ID=Master.ID_user JOIN Group_type ON Organized_course.ID_groupType=Group_type.ID WHERE Course_registration.ID='$id_status_select'";
+    $findResult = mysqli_query($link, $findQuery) or die("Ошибка " . mysqli_error($link));
+
+    if($findResult){
+        $org_course = mysqli_fetch_assoc($findResult);
+        $id_orgCourse = $org_course['id_orgCourse'];
+        $user_id = $org_course['user_id'];
+
+        $query2 = "SELECT email FROM User WHERE ID='$user_id'";
+		$result2 = mysqli_query($link, $query2) or die("Ошибка " . mysqli_error($link));
+
+		if($result2){
+			$user=mysqli_fetch_row($result2);
+			$email_user=$user[0];
+		}
+
+        $scheduleQuery = "SELECT * FROM Courses_schedule JOIN DateTime_class ON Courses_schedule.ID_dateTimeClass=DateTime_class.ID  WHERE Courses_schedule.ID_organizedCourse=$id_orgCourse";
+        $scheduleResult = mysqli_query($link, $scheduleQuery) or die("Ошибка".mysqli_error($link));
+
+        $course_shedule= "";
+        if($scheduleResult) 
+        {   
+            $rows3 = mysqli_num_rows($scheduleResult);
+            for($s = 0; $s < $rows3; ++$s) 
+            {
+                $schedule = mysqli_fetch_assoc($scheduleResult); 
+                $course_shedule.=" ".$schedule['day']."-".date('H:i', strtotime($schedule['time']))."\n ";
+            }
+            mysqli_free_result($scheduleResult);
+        }
+
+        $body="<h2>Статус заявки на ".$org_course['title']." изменён на «‎".$status."»</h2>\n
+	    <b>Начало курса: </b>".date('d.m.Y', strtotime($org_course['startDate']))."<br>
+	    <b>График: </b>".$course_shedule."\n
+	    <b>Преподаватель: </b>".$org_course['name']."<br>
+	    <b>Группа: </b>".$org_course['groupType']."<br>
+	    <b>Стоимость: </b>".($org_course['price']*$org_course['priceCoefficient'])." BYN.<br><br>
+	    
+	    Отменить заявку можно в Вашем личном кабинете.<br>
+	    Посмотреть подробности о курсе можно на сайте Flowress.by<br>
+	    Связаться с нами можно по почте Flowress_beauty_school@gmail.com и по номеру телефона +375 (29) 632-14-22 <br>
+	    <br><br>
+
+	    Благодарим, что выбрали нас!";
+
+	    send_mail($settings['mail_settings_prod'], [$email_user], 'Письмо с сайта Flowress', $body);
+    } 
 }
 
 echo "<div class='course_status col-2' id='course_status".$id_status_select."'>".$status."</div>";
--- modules/pages_handlers/apply_course_handler.php
+++ modules/pages_handlers/apply_course_handler.php
@@ -11,6 +11,7 @@ if(isset($_SESSION['user']['id'])) {
 
 	$id_orgCourse=$_POST["id_org_course"];
 	$id_user=$_SESSION['user']['id'];
+    $email_user=$_SESSION['user']['email'];
 
 	$query = "SELECT * FROM Course_registration WHERE ID_user='$id_user' AND ID_organizedCourse='$id_orgCourse'";
 	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
@@ -24,13 +25,45 @@ if(isset($_SESSION['user']['id'])) {
     		$query = "INSERT INTO Course_registration (ID_user, ID_organizedCourse, ID_status) VALUES ($id_user, $id_orgCourse, 1)";
         	$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
 
+            $findQuery = "SELECT Organized_course.startDate, User.name, Course.title, Course.price, Group_type.groupType, Group_type.priceCoefficient FROM Organized_course JOIN Course ON Organized_course.ID_course=Course.ID JOIN Master ON Organized_course.ID_master=Master.ID JOIN User ON User.ID=Master.ID_user JOIN Group_type ON Organized_course.ID_groupType=Group_type.ID WHERE Organized_course.ID='$id_orgCourse'";
+            $findResult = mysqli_query($link, $findQuery) or die("Ошибка " . mysqli_error($link));
+
+            if($findResult){
+                $org_course = mysqli_fetch_assoc($findResult);
+
+                $scheduleQuery = "SELECT * FROM Courses_schedule JOIN DateTime_class ON Courses_schedule.ID_dateTimeClass=DateTime_class.ID  WHERE Courses_schedule.ID_organizedCourse=$id_orgCourse";
+                $scheduleResult = mysqli_query($link, $scheduleQuery) or die("Ошибка".mysqli_error($link));
+
+                $course_shedule= "";
+                if($scheduleResult) 
+                {   
+                    $rows3 = mysqli_num_rows($scheduleResult);
+                    for($s = 0; $s < $rows3; ++$s) 
+                    {
+                        $schedule = mysqli_fetch_assoc($scheduleResult); 
+                        $course_shedule.=" ".$schedule['day']."-".date('H:i', strtotime($schedule['time']))."\n ";
+                    }
+                    mysqli_free_result($scheduleResult);
+                }
+
+                $body="<h2>Ваша заявка на ".$org_course['title']." отправлена</h2>\n
+                <b>Начало курса: </b>".date('d.m.Y', strtotime($org_course['startDate']))."<br>
+                <b>График: </b>".$course_shedule."\n
+                <b>Преподаватель: </b>".$org_course['name']."<br>
+                <b>Группа: </b>".$org_course['groupType']."<br>
+                <b>Стоимость: </b>".($org_course['price']*$org_course['priceCoefficient'])." BYN.<br><br>
+                
+                Отменить заявку можно в Вашем личном кабинете.<br>
+                Посмотреть подробности о курсе можно на сайте Flowress.by<br>
+                Связаться с нами можно по почте Flowress_beauty_school@gmail.com и по номеру телефона +375 (29) 632-14-22 <br>
+                <br><br>
+
+                Благодарим, что выбрали нас!";
+
+                send_mail($settings['mail_settings_prod'], [$email_user], 'Письмо с сайта Flowress', $body);
+            }
+
         	$status_id="заявка отправлена";
-
-            $body="<h1>Ваша заявка на курс отправлена</h1>\n
-            Посмотреть подробности о курсе можноо в личном кабинете";
-
-            send_mail($settings['mail_settings_prod'], ['katevoits06@gmail.com'], 'Письмо с сайта Flowress', $body);
-
     	} 
 
         echo "<div class='status'>".$status_id."</div>";
